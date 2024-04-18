3a8e8095d6837099de94c195f9e5f5ae137b43ae
Jack.Ji <jack.ji@homecredit.cn>
2024-04-18 09:19:51 +0800

    add new win app for autostart

---
+++ 05_opslocal/_01_open_win_app.py
@@ -0,0 +1,11 @@
+import os
+
+os.popen("D:/oth_tools/Snipaste-1.16.2-x64/Snipaste.exe")
+os.popen("D:/oth_tools/Ditto/Ditto.exe")
+os.popen("D:/oth_tools/WXWork/WXWork.exe")
+os.popen("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe")
+os.popen("D:/dev_tools/ideaIU-2020.3.4.win/bin/idea64.exe")
+os.popen("C:/Program Files (x86)/Cisco/Cisco AnyConnect Secure Mobility Client/vpnui.exe")
+os.popen("D:/oth_tools/Listary/Listary.exe")
+os.popen("D:/oth_tools/dev-sidecar/dev-sidecar.exe")
+os.popen("D:/oth_tools/Microsoft VS Code/Code.exe")
