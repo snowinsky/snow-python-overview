50acf3e3606a631c500cebc5d1378a7a0bb8b8ae
Jack.Ji <jack.ji@homecredit.cn>
2024-05-31 10:00:53 +0800

    s3 download file

--- 06_project/_13_s3cmder/s3_sdk_client.py
+++ 06_project/_13_s3cmder/s3_sdk_client.py
@@ -33,23 +33,23 @@ class Boto3_S3_Client(object):
     def list_objects_by_page(self, bucket_name='k8s-log', key_words=None):
         paginator = self.s3_client.get_paginator('list_objects_v2')
 
-        with open("bank-vendor_jetpay__log.txt", 'w') as f:
-            # Create a PageIterator from the Paginator
-            page_iterator = paginator.paginate(Bucket=bucket_name, Prefix='newmetalog')
-            obj_key_list = []
-            page = 1
-            for objs in page_iterator:
-                print("批量处理中，，第", page, '页', end='\n')
-                page += 1
-                for obj in objs['Contents']:
-                    obj_key = obj['Key']
-                    if key_words:
-                        if all(key_word in obj_key for key_word in key_words):
-                            f.write(obj_key + '\n')
-                            obj_key_list.append(obj_key)
-                    else:
+        # with open("bank-vendor_jetpay__log.txt", 'w') as f:
+        # Create a PageIterator from the Paginator
+        page_iterator = paginator.paginate(Bucket=bucket_name, Prefix='newmetalog')
+        obj_key_list = []
+        page = 1
+        for objs in page_iterator:
+            print("批量处理中，，第", page, '页', end='\n')
+            page += 1
+            for obj in objs['Contents']:
+                obj_key = obj['Key']
+                if key_words:
+                    if all(key_word in obj_key for key_word in key_words):
+                        # f.write(obj_key + '\n')
                         obj_key_list.append(obj_key)
-            return obj_key_list
+                else:
+                    obj_key_list.append(obj_key)
+        return obj_key_list
 
     def list_objects_in_bucket(self, bucket_name='k8s-log', key_words=None):
         objs = self.s3_client.list_objects_v2(Bucket=bucket_name, Prefix='newmetalog')
@@ -100,5 +100,5 @@ if __name__ == '__main__':
     # obj_key_list = s3resource.list_objects_in_bucket(bucket_name='k8s-log', key_words=('newmetalog', '2023/12/31', 'cashier-wechat-api'))
     # print(obj_key_list)
     # print(s3client.list_log_file('k8s-log','2023/12/31', 'cashier-wechat-api'))
-    print(s3client.list_objects_by_page(bucket_name='k8s-log', key_words=('bank-vendor', 'jetpay')))
+    print(s3client.list_objects_by_page(bucket_name='k8s-log', key_words=('dddt', 'cashier-wechat')))
 
